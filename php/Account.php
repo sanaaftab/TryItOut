61760d8dce7c57b386e4b0dcc2ab7cc0f6574cc2
Arron Fox <arron.fox@student.manchester.ac.uk>
2019-03-04 19:38:22 +0000

    Upload New File

---
+++ php/Account.php
@@ -0,0 +1,17 @@
+<?php
+	ini_set('display_errors', 1);
+	$hostname = "dbhost.cs.man.ac.uk";
+	$mysqlusername = "n33565af";
+	$mysqlpassword = "databasepass";
+	$dbName = "n33565af";
+	
+	//displays Recent outfit, favourite items(create new attribute),
+	//make connection
+	$connection = new mysqli($hostname, $mysqlusername, $password, $dbName);
+	
+	if(!$connection)
+	{
+		die("Connection failed. ". mysqli_connect_error());
+	}		
+	
+?>
