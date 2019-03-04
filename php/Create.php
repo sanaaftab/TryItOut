9fe2cdfa7beede4001ed6610a7ccfb34b6233b62
Arron Fox <arron.fox@student.manchester.ac.uk>
2019-03-04 19:37:08 +0000

    Upload New File

---
+++ php/Create.php
@@ -0,0 +1,12 @@
+<?php
+	ini_set('display_errors', 1);
+	$hostname = "dbhost.cs.man.ac.uk";
+	$mysqlusername = "n33565af";
+	$mysqlpassword = "databasepass";
+	$dbName = "n33565af";
+	
+	//loads new items
+	//saves the outfit
+	//possibly remove page as same script as newitems may be useful, create seperate save outfit script
+	
+?>
