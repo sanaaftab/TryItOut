3c87f9d20b89c6f2cfb00905178a616227952870
Arron Fox <arron.fox@student.manchester.ac.uk>
2019-03-04 19:38:32 +0000

    Upload New File

---
+++ php/Settings.php
@@ -0,0 +1,11 @@
+<?php
+	ini_set('display_errors', 1);
+	$hostname = "dbhost.cs.man.ac.uk";
+	$mysqlusername = "n33565af";
+	$mysqlpassword = "databasepass";
+	$dbName = "n33565af";
+	
+	//upload dp, change password, logout
+	
+	
+?>
