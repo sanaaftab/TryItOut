8d1e17d1d2e22de7f8b41edf8c664a93f774045d
Nikolay Avramov <nikolay.avramov@student.manchester.ac.uk>
2019-03-18 16:47:55 +0000

    php updates

--- website/CheckLogin.php
+++ website/CheckLogin.php
@@ -4,45 +4,56 @@
 	$mysqlusername = "n33565af";
 	$mysqlpassword = "databasepass";
 	$dbName = "2018_comp10120_w1";
-	
-//this script compares the user input to associated login info in the User entity and then if it is correct will allow the user to login 	
+
+//this script compares the user input to associated login info in the User entity and then if it is correct will allow the user to login
 	//declaring vars being passed from client
 	$Email = $_POST["EmailPost"];
 	$Password = $_POST["PasswordPost"];
-	
-	
+
+
 	//make connection
 	$connection = new mysqli($hostname, $mysqlusername, $mysqlpassword, $dbName);
 	if(!$connection)
 	{
 		die("Connection failed. ". mysqli_connect_error());
 	}
-	
+
 	//query to select the password of the username entered
 	//preparing parameters to prevent sql injection
 	$query = mysqli_prepare($connection, "SELECT Password FROM USERS WHERE Email = ?;");
 	//binding parameters
-	$insert = mysqli_stmt_bind_param($query, "s", $Email);
-	
+  mysqli_stmt_bind_param($query, "s", $Email);
+
 	//executes query
 	mysqli_stmt_execute($query);
-	//binds results 
+	//binds results
 	mysqli_stmt_bind_result($query, $result);
 	//stores results in var
 	$row = mysqli_stmt_fetch($query);
 	//closes statement
-	mysqli_stmt_close($query);			
-		
+	mysqli_stmt_close($query);
+
+	$uIDQuery = mysqli_prepare($connection, "SELECT UserID FROM USERS WHERE Email = ?;");
+
+	mysqli_stmt_bind_param($uIDQuery, "s", $Email);
+
+	mysqli_stmt_execute($uIDQuery);
+
+	mysqli_stmt_bind_result($uIDQuery, $uIDResult);
+
+	$uID = mysqli_stmt_fetch($uIDQuery)
 	//checks to see if the password entered is the same as the original password
 	if($Password == $row)
 	{
 		echo "login successful";
+		session_start();
+		$_SESSION['uID'] = $uID;
 		header("Location: explore.html");
-	}	
-	else 
+	}
+	else
 	{
 		echo "Password incorrect, try again";
 		header("Location: login.html");
-	}		
-	
+	}
+
 ?>
