02b3e5d2b9c5b6fa748bcb17d76daf6543d71a93
Arron Fox <arron.fox@student.manchester.ac.uk>
2019-03-20 16:04:27 +0000

    Update website/create.php

--- website/create.php
+++ website/create.php
@@ -191,7 +191,7 @@
 		image.style.height = 'auto';
 		image.style.width = 200;
     image.draggable = 'true';
-    image.ondragstart = 'drag(event)';
+    image.addEventListener("dragstart", drag(event));
 
 		let div3 = document.createElement("div");
 		div3.className = "card-footer";
