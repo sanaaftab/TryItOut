40cc1e8650f99c8d6cc87b7410a3c28045afa28c
Arron Fox <arron.fox@student.manchester.ac.uk>
2019-03-21 14:18:40 +0000

    Update website/create.php

--- website/create.php
+++ website/create.php
@@ -204,7 +204,7 @@
         <div class="columnright">
 
           <div>
-            <button class="btn" onclick="displayAll(clothesObjArray)" >SHOW ALL</button>
+            <button class="btn active" onclick="displayAll(clothesObjArray)" >SHOW ALL</button>
             <button class="btn" onclick="displayAll(favClothesObjArray)" >FAVORITES</button>
           </div>
           <div class="scroll">
